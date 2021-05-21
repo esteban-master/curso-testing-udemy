2fb2d1a2b516834e7db5fefea8936ec8f86a96fb
Esteban Beltran <estebanenriquebeltran@gmail.com>
2021-05-20 22:44:14 -0400

    modificando app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <h1>Hola</h1>
+      <h1>Hola Esteban</h1>
     </div>
   );
 }
